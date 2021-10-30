fc214f6932e4bee1d92227cb449c67b213e153a4
walmir91 <walmirlmjr@gmail.com>
2021-10-30 12:05:41 -0300

    Repositorios implementados

---
+++ src/main/java/com/walmir/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.walmir.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.walmir.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer>{
+
+}
