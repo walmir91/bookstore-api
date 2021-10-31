1fc8ee3fd27bd28c87cdc25934c6ed39686a154f
walmir91 <walmirlmjr@gmail.com>
2021-10-31 15:45:11 -0300

    Criação POST para livros

--- src/main/java/com/walmir/bookstore/service/LivroService.java
+++ src/main/java/com/walmir/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.walmir.bookstore.domain.Categoria;
 import com.walmir.bookstore.domain.Livro;
 import com.walmir.bookstore.exceptions.ObjectNotFoundException;
 import com.walmir.bookstore.repositories.LivroRepository;
@@ -46,5 +47,12 @@ public class LivroService {
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
 		
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 }
